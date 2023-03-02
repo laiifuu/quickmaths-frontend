3729d824150a585ff008fe91de79d7e66ada8d1e
Daron976 <daronkwiafe@gmail.com>
2023-03-02 18:45:49 +0200

    Implement functionality after successful creation of tutor

--- src/modules/user-actions/addItem.js
+++ src/modules/user-actions/addItem.js
@@ -1,9 +1,9 @@
 /*eslint-disable*/
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { createTutor } from '../../redux/user/additem-redux';
+import { aftermath, createTutor } from '../../redux/user/additem-redux';
 import './additem.scss';
 
 const AddItem = () => {
@@ -17,11 +17,12 @@ const AddItem = () => {
     handleSubmit,
     formState: { errors },
   } = useForm({
-    mode: "all"
+    mode: "onChange"
   });
 
   const redirection = useNavigate();
 
+  const returnMsg = useSelector((state) => state.createTutorReducer)
   // const [first_name, setFName] = useState('');
   // const [last_name, setLName] = useState('');
   // const [photo_url, setImage] = useState('');
@@ -39,149 +40,169 @@ const AddItem = () => {
   // };
 
   const postData = (data) => {
-    // dispatch(createTutor(obj));
+    dispatch(createTutor(data));
     // localStorage.setItem('created', true);
     console.log(data);
   };
 
-  // useEffect(() => {
-  //   if (localStorage.getItem('created')) {
-  //     setOverlay(true);
-  //     localStorage.removeItem('created');
-  //     setTimeout(() => {
-  //       redirection('/');
-  //     }, 2500);
-  //   }
-  // }, [redirection]);
+  useEffect(() => {
+    if (returnMsg.message) {
+      if (returnMsg.message === 'Tutor has been created successfully!') {
+        // console.log(returnMsg)
+        setOverlay(true);
+        setTimeout(() => {
+          dispatch(aftermath());
+          redirection('/');
+        }, 2500);
+      } else if (returnMsg.message === "Tutor couldn't be created.") {
+        console.log(returnMsg)
+        setOverlay(true);
+        setTimeout(() => {
+          setOverlay(false);
+          dispatch(aftermath());
+          // redirection('/');
+        }, 2500);
+      }
+      setOverlay(true);
+    }
+  });
 
   return (
     <section className="add-item flex">
-      <div>
+      <div className='add-item-header'>
         <h1>Add a tutor</h1>
+        <p>(* indicates a required field)</p>
       </div>
       <form action="" className="user-form flex" onSubmit={handleSubmit(postData)}>
-        <label htmlFor="first_name" className="flex">
-          First Name *
-          <input
-            type="input"
-            name="first_name"
-            placeholder='First Name'
-            {...register('first_name',{
-              required: true,
-              pattern: {
-                value: /[A-Za-z]/,
-                message: 'First name can ony include letters'
-              }
-            })}
-          />
-        </label>
-        {errors.first_name && <p className="errorMsg">{errors.first_name.message}</p>}
-        <label htmlFor="last_name" className="flex">
-          Last Name *
-          <input
-            type="input"
-            name="last_name"
-            placeholder='Last Name'
-            {...register('last_name',{
-              required: true,
-              pattern: {
-                value: /[A-Za-z]/,
-                message: 'Last name can ony include letters'
-              }
-            })}
-          />
-        </label>
-        {errors.last_name && <p className="errorMsg">{errors.last_name.message}</p>}
-        <label htmlFor="photo_url" className="flex">
-          <p className='photo-url'>Photo<em>{' '}(URL)</em> *</p>
-          <input
-            type="input"
-            name="photo_url"
-            placeholder='Photo URL'
-            {...register('photo_url',{
-              required: true
-            })}
-          />
-        </label>
-        <label htmlFor="description" className="flex">
-          Description *
-          <input
-            type="text"
-            name="description"
-            placeholder='Description'
-            {...register('description',{
-              required: true,
-              minLength: {
-                value: 20,
-                message: 'Description must have a minimum of 20 characters'
-              },
-              maxLength: {
-                value: 120,
-                message: 'Description can only have a maximum of 120 characters'
-              },
-            })}
-          />
-        </label>
-        {errors.description && <p className="errorMsg">{errors.description.message}</p>}
-        <label htmlFor="hourly_fee" className="flex">
-          Hourly fee *
-          <input
-            type="number"
-            name="hourly_fee"
-            placeholder='Hourly Fee'
-            {...register('hourly_fee',{
-              required: true,
-              min: {
-                value: 0,
-                message: "Hourly fee must be greater than 0"
-              }
-            })}
-          />
-        </label>
-        {errors.hourly_fee && <p className="errorMsg">{errors.hourly_fee.message}</p>}
-        <label htmlFor="experience" className="flex">
-          Experience *
-          <input
-            type="number"
-            name="experience"
-            placeholder='Experience'
-            {...register('experience',{
-              required: true,
-              min: {
-                value: 0,
-                message: "Experience must be greater than 0"
-              }
-            })}
-          />
-        </label>
-        {errors.experience && <p className="errorMsg">{errors.experience.message}</p>}
-        <label htmlFor="ig_link" className="flex">
-          Instagram
-          <input
-            type="input"
-            name="ig_link"
-            placeholder='Instagram link'
-            {...register('ig_link')}
-          />
-        </label>
-        <label htmlFor="twitter_link" className="flex">
-          Twitter
-          <input
-            type="input"
-            name="twitter_link"
-            placeholder='Twitter link'
-            {...register('twitter_link')}
-          />
-        </label>
-        <label htmlFor="fb_link" className="flex">
-          FaceBook
-          <input
-            type="input"
-            name="fb_link"
-            placeholder='Facebook link'
-            {...register('fb_link')}
-          />
-        </label>
+        <div className='wrapper'>
+
+          <div className='container flex'>
+            <label htmlFor="first_name" className="flex">
+              First Name *
+              <input
+                type="input"
+                name="first_name"
+                placeholder='First Name'
+                {...register('first_name',{
+                  required: true,
+                  pattern: {
+                    value: /[A-Za-z]/,
+                    message: 'First name can ony include letters'
+                  }
+                })}
+              />
+            </label>
+            {errors.first_name && <p className="errorMsg">{errors.first_name.message}</p>}
+            <label htmlFor="last_name" className="flex">
+              Last Name *
+              <input
+                type="input"
+                name="last_name"
+                placeholder='Last Name'
+                {...register('last_name',{
+                  required: true,
+                  pattern: {
+                    value: /[A-Za-z]/,
+                    message: 'Last name can ony include letters'
+                  }
+                })}
+              />
+            </label>
+            {errors.last_name && <p className="errorMsg">{errors.last_name.message}</p>}
+            <label htmlFor="photo_url" className="flex">
+              <p className='photo-url'>Photo<em>{' '}(URL)</em> *</p>
+              <input
+                type="input"
+                name="photo_url"
+                placeholder='Photo URL'
+                {...register('photo_url',{
+                  required: true
+                })}
+              />
+            </label>
+            <label htmlFor="description" className="flex">
+              Description *
+              <input
+                type="text"
+                name="description"
+                placeholder='Description'
+                {...register('description',{
+                  required: true,
+                  minLength: {
+                    value: 20,
+                    message: 'Description must have a minimum of 20 characters'
+                  },
+                  maxLength: {
+                    value: 120,
+                    message: 'Description can only have a maximum of 120 characters'
+                  },
+                })}
+              />
+            </label>
+            {errors.description && <p className="errorMsg">{errors.description.message}</p>}
+            <label htmlFor="hourly_fee" className="flex">
+              Hourly fee *
+              <input
+                type="number"
+                name="hourly_fee"
+                placeholder='Hourly Fee'
+                {...register('hourly_fee',{
+                  required: true,
+                  min: {
+                    value: 0,
+                    message: "Hourly fee must be greater than 0"
+                  }
+                })}
+              />
+            </label>
+            {errors.hourly_fee && <p className="errorMsg">{errors.hourly_fee.message}</p>}
+          </div>
+          <div className='container flex'>
+            <label htmlFor="experience" className="flex">
+              Experience *
+              <input
+                type="number"
+                name="experience"
+                placeholder='Experience'
+                {...register('experience',{
+                  required: true,
+                  min: {
+                    value: 0,
+                    message: "Experience must be greater than 0"
+                  }
+                })}
+              />
+            </label>
+            {errors.experience && <p className="errorMsg">{errors.experience.message}</p>}
+            <label htmlFor="ig_link" className="flex">
+              Instagram
+              <input
+                type="input"
+                name="ig_link"
+                placeholder='Instagram link'
+                {...register('ig_link')}
+              />
+            </label>
+            <label htmlFor="twitter_link" className="flex">
+              Twitter
+              <input
+                type="input"
+                name="twitter_link"
+                placeholder='Twitter link'
+                {...register('twitter_link')}
+              />
+            </label>
+            <label htmlFor="fb_link" className="flex">
+              FaceBook
+              <input
+                type="input"
+                name="fb_link"
+                placeholder='Facebook link'
+                {...register('fb_link')}
+              />
+            </label>
+          </div>
+        </div>
         <button
           type="submit"
           name="additem"
@@ -190,14 +211,14 @@ const AddItem = () => {
           Add tutor
         </button>
       </form>
-      {/* <div
+      <div
         className="overlay"
         style={{
           display: overlay ? 'flex' : 'none',
         }}
       >
-        <p>Tutor successfully created!</p>
-      </div> */}
+        <p>{returnMsg.message}</p>
+      </div>
       {/* <div
         className='overlay'
         style={{
--- src/redux/user/additem-redux.js
+++ src/redux/user/additem-redux.js
@@ -6,9 +6,15 @@ const APIURL = 'http://127.0.0.1:3000/api/v1/tutors';
 
 const FULLFILED = 'quickmaths-frontend/user/additem-redux/FULLFILED';
 
+const AFTERMATH = 'quickmaths-frontend/user/additem-redux/AFTERMATH';
+
 export const fullfiled = (obj) => ({
   type: FULLFILED,
   payload: { obj },
+});
+
+export const aftermath = () => ({
+  type: AFTERMATH,
 });
 
 export const createTutor = (obj) => async (dispatch) => fetch(APIURL, {
@@ -27,6 +33,8 @@ const createTutorReducer = (state = [], action) => {
   switch (action.type) {
     case FULLFILED:
       return action.payload.obj;
+    case AFTERMATH:
+      return [];
     default:
       return state;
   }

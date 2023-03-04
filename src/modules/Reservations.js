978e9d6d4568c3db3394e5caf90be500424bee81
vflorin1995 <vflorin1995@gmail.com>
2023-03-04 21:31:57 +0200

    Display reservations

--- src/modules/Reservations.js
+++ src/modules/Reservations.js
@@ -1,5 +1,30 @@
+import { useSelector } from 'react-redux';
+import './Reservations.scss';
+
 const Reservations = () => {
-  const x = 0;
-  return (<div>REservations</div>);
+  const reservations = useSelector((state) => state.users.reservations);
+  const tutors = useSelector((state) => state.tutors.tutors);
+  console.log(reservations);
+  console.log(tutors);
+  return (
+    <table>
+      <tr>
+        <th>Tutor Name</th>
+        <th>City</th>
+        <th>Date</th>
+      </tr>
+      {reservations.map((item) => (
+        <tr key={item.id}>
+          <td>
+            {tutors.find((tutor) => tutor.id === item.tutor_id).firstName}
+            {' '}
+            {tutors.find((tutor) => tutor.id === item.tutor_id).lastName}
+          </td>
+          <td>{item.city}</td>
+          <td>{item.date}</td>
+        </tr>
+      ))}
+    </table>
+  );
 };
 export default Reservations;

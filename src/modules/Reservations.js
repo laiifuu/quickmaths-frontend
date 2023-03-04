9372ce83184fdd158e0af4f24a4f5a6238dee352
vflorin1995 <vflorin1995@gmail.com>
2023-03-04 23:41:23 +0200

    Sort reservations cronologically

--- src/modules/Reservations.js
+++ src/modules/Reservations.js
@@ -2,29 +2,34 @@ import { useSelector } from 'react-redux';
 import './Reservations.scss';
 
 const Reservations = () => {
-  const reservations = useSelector((state) => state.users.reservations);
+  // eslint-disable-next-line max-len
+  const reservations = useSelector((state) => state.users.reservations).sort((a, b) => { if (a.date > b.date && a.hour.match(/\d+/) > b.hour.match(/\d+/)) return 1; return -1; });
   const tutors = useSelector((state) => state.tutors.tutors);
   console.log(reservations);
   console.log(tutors);
   return (
-    <table>
-      <tr>
-        <th>Tutor Name</th>
-        <th>City</th>
-        <th>Date</th>
-      </tr>
-      {reservations.map((item) => (
-        <tr key={item.id}>
-          <td>
-            {tutors.find((tutor) => tutor.id === item.tutor_id).firstName}
-            {' '}
-            {tutors.find((tutor) => tutor.id === item.tutor_id).lastName}
-          </td>
-          <td>{item.city}</td>
-          <td>{item.date}</td>
+    <>
+      <table>
+        <tr>
+          <th>Tutor Name</th>
+          <th>City</th>
+          <th>Date</th>
+          <th>Hour</th>
         </tr>
-      ))}
-    </table>
+        {reservations.map((item) => (
+          <tr key={item.id}>
+            <td>
+              {tutors.find((tutor) => tutor.id === item.tutor_id).firstName}
+              {' '}
+              {tutors.find((tutor) => tutor.id === item.tutor_id).lastName}
+            </td>
+            <td>{item.city}</td>
+            <td>{item.date}</td>
+            <td>{item.hour}</td>
+          </tr>
+        ))}
+      </table>
+    </>
   );
 };
 export default Reservations;

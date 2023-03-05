0cf8ff8608c06a18f74367fceddfdbfe91b77465
vflorin1995 <vflorin1995@gmail.com>
2023-03-05 15:39:53 +0200

    Add redirect if user is not logged in

--- src/modules/Reservations.js
+++ src/modules/Reservations.js
@@ -1,54 +1,74 @@
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import './Reservations.scss';
 
 const Reservations = () => {
-  const reservations = useSelector((state) => state.users.reservations).sort((a, b) => {
-    if (a.date > b.date) return 1; return -1;
-  });
+  const isLoggedIn = JSON.parse(window.localStorage.getItem('logged_in'));
+  const navigate = useNavigate();
+
+  const sortReservations = (a, b) => {
+    const dateA = new Date(a.date);
+    const hourA = Number(a.hour.replace(/(^d+)(.+$)/i, '$1'));
+    dateA.setHours(hourA);
+
+    const dateB = new Date(b.date);
+    const hourB = Number(b.hour.replace(/(^d+)(.+$)/i, '$1'));
+    dateB.setHours(hourB);
+
+    return dateB - dateA;
+  };
+
+  let reservations = useSelector((state) => state.users.reservations);
   const tutors = useSelector((state) => state.tutors.tutors);
-  console.log(reservations);
-  console.log(tutors);
-  return (
-    <>
-      <h2 className="reservationPageTitle">My reservations</h2>
-      <div className="reservationPageMessage">
-        Your next lesson is on
-        {' '}
-        {((new Date(reservations[0].date)).toUTCString()).substring(0, 16)}
-        {' '}
-        at
-        {' '}
-        {reservations[0].hour}
-        {' '}
-        in
-        {' '}
-        {reservations[0].city}
-        {' '}
-        with
-        {' '}
-        {tutors.find((tutor) => tutor.id === reservations[0].tutor_id).firstName}
-      </div>
-      <table>
-        <tr>
-          <th>Tutor Name</th>
-          <th>City</th>
-          <th>Date</th>
-          <th>Hour</th>
-        </tr>
-        {reservations.map((item) => (
-          <tr key={item.id}>
-            <td>
-              {tutors.find((tutor) => tutor.id === item.tutor_id).firstName}
-              {' '}
-              {tutors.find((tutor) => tutor.id === item.tutor_id).lastName}
-            </td>
-            <td>{item.city}</td>
-            <td>{item.date}</td>
-            <td>{item.hour}</td>
+
+  console.log(isLoggedIn, 'isloggendin');
+  if (isLoggedIn) {
+    reservations = reservations.sort(sortReservations);
+    return (
+      <>
+        <h2 className="reservationPageTitle">My reservations</h2>
+        <div className="reservationPageMessage">
+          Your next lesson is on
+          {' '}
+          {((new Date(reservations[0].date)).toUTCString()).substring(0, 16)}
+          {' '}
+          at
+          {' '}
+          {reservations[0].hour}
+          {' '}
+          in
+          {' '}
+          {reservations[0].city}
+          {' '}
+          with
+          {' '}
+          {tutors.find((tutor) => tutor.id === reservations[0].tutor_id).firstName}
+        </div>
+        <table>
+          <tr>
+            <th>Tutor Name</th>
+            <th>City</th>
+            <th>Date</th>
+            <th>Hour</th>
           </tr>
-        ))}
-      </table>
-    </>
-  );
+          {reservations.map((item) => (
+            <tr key={item.id}>
+              <td>
+                {tutors.find((tutor) => tutor.id === item.tutor_id).firstName}
+                {' '}
+                {tutors.find((tutor) => tutor.id === item.tutor_id).lastName}
+              </td>
+              <td>{item.city}</td>
+              <td>{item.date}</td>
+              <td>{item.hour}</td>
+            </tr>
+          ))}
+        </table>
+      </>
+    );
+  }
+  console.log('i am here');
+  return navigate('/user/login');
 };
+
 export default Reservations;
